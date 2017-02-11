a2688544ad9ff38fa0f0f26cb52b8222d319b554
Glyn Hudson <glyn.hudson@gmail.com>
2017-02-11 15:13:03 +0000

    use UART 1 (GPIO2 Huzzah) for debug msg's

--- src/emonesp.h
+++ src/emonesp.h
@@ -1,8 +1,10 @@
 #ifndef _EMONESP_H
 #define _EMONESP_H
 
-// Uncomment to use hardware UART 1 for debug else use UART 0
-//#define DEBUG_SERIAL1
+// Uncomment to use hardware UART 1 for debug tx (GPIO2 on Huzzah) else use UART 0
+#define DEBUG_SERIAL1
+
+//------------------------------
 #ifdef DEBUG_SERIAL1
 #define DEBUG Serial1
 #else
