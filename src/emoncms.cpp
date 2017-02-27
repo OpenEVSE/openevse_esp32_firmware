296599a90f4a7d98cd3f30ca61ed36b86598ccca
Glyn Hudson <glyn.hudson@gmail.com>
2017-02-27 20:28:55 +0000

    debufg print emocms server

--- src/emoncms.cpp
+++ src/emoncms.cpp
@@ -15,18 +15,18 @@ unsigned long packets_success = 0;
 
 
 void emoncms_publish(String url){
-  if(emoncms_apikey != 0) {  
-    DEBUG.println(url); delay(10);
+  if(emoncms_apikey != 0) {
+    DEBUG.print(emoncms_server.c_str() + String(url));
     packets_sent++;
     // Send data to Emoncms server
     String result="";
     if (emoncms_fingerprint!=0){
       // HTTPS on port 443 if HTTPS fingerprint is present
-      DEBUG.println("HTTPS Enabled"); delay(10);
+      DEBUG.println("HTTPS"); delay(10);
       result = get_https(emoncms_fingerprint.c_str(), emoncms_server.c_str(), url, 443);
     } else {
       // Plain HTTP if other emoncms server e.g EmonPi
-      DEBUG.println("Plain old HTTP"); delay(10);
+      DEBUG.println("HTTP"); delay(10);
       result = get_http(emoncms_server.c_str(), url);
     }
     if (result == "ok"){
