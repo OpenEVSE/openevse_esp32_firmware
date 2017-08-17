96c633d0d88bf1106f256627d2c9ab45bb76e411
Glyn Hudson <glyn.hudson@gmail.com>
2017-08-17 01:37:32 +0100

    make start on creating drop down pick list for energy limit

--- src/data/config.js
+++ src/data/config.js
@@ -7,7 +7,7 @@
 // and run the HTML/JS from file, no need to upload to the ESP to test
 
 var baseHost = window.location.hostname;
-//var baseHost = "openevse.local";
+// var baseHost = "openevse.local";
 //var baseHost = "192.168.4.1";
 //var baseHost = "172.16.0.70";
 
@@ -344,7 +344,7 @@ function OpenEvseViewModel(baseEndpoint, rapiViewModel) {
     { name: "2", value: 2 }];
   self.currentLevels = ko.observableArray([]);
   self.timeLimits = [
-    { name: "off", value: 0 },
+    { name: "none", value: 0 },
     { name: "15 min", value: 15 },
     { name: "30 min", value: 30 },
     { name: "45 min", value: 45 },
@@ -358,7 +358,33 @@ function OpenEvseViewModel(baseEndpoint, rapiViewModel) {
     { name: "6 hours", value: 6 * 60 },
     { name: "7 hours", value: 7 * 60 },
     { name: "8 hours", value: 8 * 60 }];
-
+  
+  self.chargeLimits = [
+    { name: "none", value: 0 },
+    { name: "1 kWh", value: 1 },
+    { name: "2 kWh", value: 2 },
+    { name: "3 kWh", value: 3 },
+    { name: "4 kWh", value: 4 },
+    { name: "5 kWh", value: 5 },
+    { name: "6 kWh", value: 6 },
+    { name: "7 kWh", value: 7 },
+    { name: "8 kWh", value: 8 },
+    { name: "9 kWh", value: 9 },
+    { name: "10 kWh", value: 10 },
+    { name: "15 kWh", value: 11 },
+    { name: "20 kWh", value: 12 },
+    { name: "25 kWh", value: 25 },
+    { name: "30 kWh", value: 30 },
+    { name: "35 kWh", value: 35 },
+    { name: "40 kWh", value: 40 },
+    { name: "45 kWh", value: 45 },
+    { name: "50 kWh", value: 50 },
+    { name: "55 kWh", value: 55 },
+    { name: "60 kWh", value: 60 },
+    { name: "70 kWh", value: 70 },
+    { name: "80 kWh", value: 80 },
+    { name: "90 kWh", value: 90 }];
+  
   self.serviceLevel = ko.observable(-1);
   self.actualServiceLevel = ko.observable(-1);
   self.minCurrentLevel = ko.observable(-1);
