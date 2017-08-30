3dbf335743348b8ab61dab3628d75958f0771254
Jeremy Poulter <jeremy@bigjungle.net>
2017-08-30 16:49:12 +0100

    Use dependencies to controll building of the minified files.
    This stops the files being built if nothing has changed.

--- scripts/extra_script.py
+++ scripts/extra_script.py
@@ -1,4 +1,4 @@
-import os.path
+from os.path import join, isfile
 import json
 from pprint import pprint
 
@@ -15,6 +15,7 @@ Import("env")
 #
 
 import httplib, urllib, sys
+
 def js_minify(original):
     params = urllib.urlencode([
         ('js_code', original),
@@ -32,40 +33,52 @@ def js_minify(original):
     conn.close()
     return data
 
-def process_app_file(source_dir, source_files, dest):
+def minify(env, target, source):
     output = ""
-    for file in source_files:
-        source_file = os.path.join(source_dir, file)
+    for source_file in source:
         print("Reading {}".format(source_file))
-        with open(source_file) as source_fh:
+        file = source_file.get_abspath()
+        with open(file) as source_fh:
             original = source_fh.read().decode('utf-8')
         if file.endswith(".css"):
             output += css_minify(original, wrap=False, comments=False, sort=True)
         elif file.endswith(".js"):
             output += js_minify(original)
             #output += original
-        elif file.endswith(".htm") or file.endswith(".html"):
+        elif file.endswith(".htm") or source_file.endswith(".html"):
             output += html_minify(original, comments=False)
-    print("Writing {}".format(dest))
-    with open(dest, "w") as output_file:
+    target_file = target[0].get_abspath()
+    print("Writing {}".format(target_file))
+    with open(target_file, "w") as output_file:
         output_file.write(output.encode('utf-8'))
 
-def process_html_app(source, dest):
-    manifest = os.path.join(source, "manifest.json")
-    if os.path.isfile(manifest):
+def process_app_file(source_dir, source_files, dest, env):
+    source = []
+    for file in source_files:
+        source.append(join(source_dir, file))
+    env.Depends("$BUILD_DIR/spiffs.bin", env.Command(dest, source, minify))
+
+        #env.VerboseAction('"$PYTHONEXE" "%s" $TARGET %s' % (join(env.subst("$PROJECT_DIR"), "scripts", "minify.py"), " ".join(source)), "Generating $TARGET")))
+
+def process_html_app(source, dest, env):
+    manifest = join(source, "manifest.json")
+    if isfile(manifest):
         with open(manifest) as manifest_file:
             man = json.load(manifest_file)
         for out_file in man:
             in_files = man[out_file]
-            process_app_file(source, in_files, os.path.join(dest, out_file))
-
+            process_app_file(source, in_files, join(dest, out_file), env)
 
 #
 # Generate SPIFFS
 #
 
-def before_spiffs(source, target, env):
-    htmlSrc = os.path.join(env.subst("$PROJECTSRC_DIR"), "html")
-    process_html_app(htmlSrc, source[0].get_abspath())
+#def before_spiffs(source, target, env):
+#    htmlSrc = join(env.subst("$PROJECTSRC_DIR"), "html")
+#    process_html_app(htmlSrc, source[0].get_abspath(), env)
 
-env.AddPreAction("$BUILD_DIR/spiffs.bin", before_spiffs)
+#env.AddPreAction("$BUILD_DIR/spiffs.bin", before_spiffs)
+
+htmlSrc = join(env.subst("$PROJECTSRC_DIR"), "html")
+dataSrc = join(env.subst("$PROJECTSRC_DIR"), "data")
+process_html_app(htmlSrc, dataSrc, env)
