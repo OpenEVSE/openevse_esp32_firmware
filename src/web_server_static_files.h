c369de9e966085131a9aa7210cd65b6dbe2d7c00
Glyn Hudson <glyn.hudson@gmail.com>
2017-11-15 02:27:18 +0000

    remove arduin IDE and spiffs from travis build

--- src/web_server_static_files.h
+++ src/web_server_static_files.h
@@ -4,10 +4,10 @@
 #include "web_server.home.htm.h"
 #include "web_server.style.css.h"
 #include "web_server.home.js.h"
-#include "web_server.emoncms.jpg.h"
 #include "web_server.favicon-32x32.png.h"
 #include "web_server.favicon-16x16.png.h"
 #include "web_server.ohm.jpg.h"
+#include "web_server.emoncms.jpg.h"
 StaticFile staticFiles[] = {
   { "/lib.js", CONTENT_LIB_JS, sizeof(CONTENT_LIB_JS) - 1, _CONTENT_TYPE_JS },
   { "/wifi_portal.htm", CONTENT_WIFI_PORTAL_HTM, sizeof(CONTENT_WIFI_PORTAL_HTM) - 1, _CONTENT_TYPE_HTML },
@@ -15,8 +15,8 @@ StaticFile staticFiles[] = {
   { "/home.htm", CONTENT_HOME_HTM, sizeof(CONTENT_HOME_HTM) - 1, _CONTENT_TYPE_HTML },
   { "/style.css", CONTENT_STYLE_CSS, sizeof(CONTENT_STYLE_CSS) - 1, _CONTENT_TYPE_CSS },
   { "/home.js", CONTENT_HOME_JS, sizeof(CONTENT_HOME_JS) - 1, _CONTENT_TYPE_JS },
-  { "/emoncms.jpg", CONTENT_EMONCMS_JPG, sizeof(CONTENT_EMONCMS_JPG) - 1, _CONTENT_TYPE_JPEG },
   { "/favicon-32x32.png", CONTENT_FAVICON_32X32_PNG, sizeof(CONTENT_FAVICON_32X32_PNG) - 1, _CONTENT_TYPE_PNG },
   { "/favicon-16x16.png", CONTENT_FAVICON_16X16_PNG, sizeof(CONTENT_FAVICON_16X16_PNG) - 1, _CONTENT_TYPE_PNG },
   { "/ohm.jpg", CONTENT_OHM_JPG, sizeof(CONTENT_OHM_JPG) - 1, _CONTENT_TYPE_JPEG },
+  { "/emoncms.jpg", CONTENT_EMONCMS_JPG, sizeof(CONTENT_EMONCMS_JPG) - 1, _CONTENT_TYPE_JPEG },
 };
