0937941744e771b576061a9577dbe127e3c50162
Jo Verbeeck <joverbeeck@outlook.com>
2018-01-10 23:02:37 +0100

    Update openevse.js
    detect if no real time clock is present and revert to browser clock and (possibly) solved issue #119 (although I can't test it as I have no RTC installed)

--- src/html/openevse.js
+++ src/html/openevse.js
@@ -185,7 +185,12 @@ function OpenEVSE(endpoint)
         var second = parseInt(data[5]);
 
         if(!isNaN(year) && !isNaN(month) && !isNaN(day) && !isNaN(hour) && !isNaN(minute) && !isNaN(second)) {
-          var date = new Date(2000+year, month-1, day, hour, minute, second);
+          if(year==165 && month==165 && day==165 && hour==165 && minute==165 && second==85){
+            //no RTC connected, use browser clock instead
+            var date = new Date();
+          } else {
+            var date = new Date(2000+year, month, day, hour, minute, second);
+          }
           callback(date);
         } else {
           request._error(new OpenEVSEError("ParseError", "Could not parse time \""+data.join(" ")+"\" arguments"));
