1ad7d7b578b1c9be40fbc4ac6b2f0ff4630daa3d
Glyn Hudson <glyn.hudson@gmail.com>
2018-02-01 01:20:47 +0000

    add temperature to session status

--- src/web_server_static_files.h
+++ src/web_server_static_files.h
@@ -6,8 +6,8 @@
 #include "web_server.home.js.h"
 #include "web_server.favicon-32x32.png.h"
 #include "web_server.favicon-16x16.png.h"
-#include "web_server.emoncms.jpg.h"
 #include "web_server.ohm.jpg.h"
+#include "web_server.emoncms.jpg.h"
 StaticFile staticFiles[] = {
   { "/lib.js", CONTENT_LIB_JS, sizeof(CONTENT_LIB_JS) - 1, _CONTENT_TYPE_JS },
   { "/wifi_portal.htm", CONTENT_WIFI_PORTAL_HTM, sizeof(CONTENT_WIFI_PORTAL_HTM) - 1, _CONTENT_TYPE_HTML },
@@ -17,6 +17,6 @@ StaticFile staticFiles[] = {
   { "/home.js", CONTENT_HOME_JS, sizeof(CONTENT_HOME_JS) - 1, _CONTENT_TYPE_JS },
   { "/favicon-32x32.png", CONTENT_FAVICON_32X32_PNG, sizeof(CONTENT_FAVICON_32X32_PNG) - 1, _CONTENT_TYPE_PNG },
   { "/favicon-16x16.png", CONTENT_FAVICON_16X16_PNG, sizeof(CONTENT_FAVICON_16X16_PNG) - 1, _CONTENT_TYPE_PNG },
-  { "/emoncms.jpg", CONTENT_EMONCMS_JPG, sizeof(CONTENT_EMONCMS_JPG) - 1, _CONTENT_TYPE_JPEG },
   { "/ohm.jpg", CONTENT_OHM_JPG, sizeof(CONTENT_OHM_JPG) - 1, _CONTENT_TYPE_JPEG },
+  { "/emoncms.jpg", CONTENT_EMONCMS_JPG, sizeof(CONTENT_EMONCMS_JPG) - 1, _CONTENT_TYPE_JPEG },
 };
