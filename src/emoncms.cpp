45d32f7a10fcfe01dcec2ab023ad2f0bf57f7d41
Jeremy Poulter <jeremy@bigjungle.net>
2019-07-09 23:21:18 +0100

    Moved Root CA to a shared file and added some other useful root CAs

--- src/emoncms.cpp
+++ src/emoncms.cpp
@@ -7,6 +7,7 @@
 #include "config.h"
 #include "http.h"
 #include "input.h"
+#include "root_ca.h"
 
 boolean emoncms_connected = false;
 
@@ -16,28 +17,6 @@ unsigned long packets_success = 0;
 const char *post_path = "/input/post?";
 
 static MongooseHttpClient client;
-
-static const char *root_ca =
-"-----BEGIN CERTIFICATE-----\r\n"
-"MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/\r\n"
-"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\r\n"
-"DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow\r\n"
-"PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD\r\n"
-"Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\r\n"
-"AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O\r\n"
-"rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq\r\n"
-"OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b\r\n"
-"xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw\r\n"
-"7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD\r\n"
-"aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV\r\n"
-"HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG\r\n"
-"SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69\r\n"
-"ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr\r\n"
-"AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz\r\n"
-"R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5\r\n"
-"JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo\r\n"
-"Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\r\n"
-"-----END CERTIFICATE-----\r\n";
 
 void emoncms_publish(String data)
 {
@@ -60,7 +39,7 @@ void emoncms_publish(String data)
     DBUGLN(url);
     packets_sent++;
 
-    client.get(url, [](MongooseHttpClientResponse *response) 
+    client.get(url, [](MongooseHttpClientResponse *response)
     {
       MongooseString result = response->body();
       if (result == "ok") {
---
+++ src/root_ca.h
@@ -0,0 +1,6 @@
+#ifndef _OPENEVSE_ROOT_CA_H
+#define _OPENEVSE_ROOT_CA_H
+
+extern const char *root_ca;
+
+#endif
