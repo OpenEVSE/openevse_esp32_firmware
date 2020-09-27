6e590a0cb6c19c9acc88c711cc98fa339270d074
Thomas Schiex <thomas.schiex@free.fr>
2020-09-27 16:59:25 +0200

    Minor change in the divert_PV_ratio semantics: values beyond 1.0 control the Grid IE reserve in kW

--- src/divert.cpp
+++ src/divert.cpp
@@ -25,25 +25,25 @@
 
 // It's better to never import current from the grid but because of charging current quantification (min value of 6A and change in steps of 1A),
 // it may be better to import a fraction of the charge current and use the next charging level sooner, depending on electricity buying/selling prices.
-// The marginal fraction of current that is required to come from PV is the divert_PV_ratio. The default behavior of requiring a pure PV charge is obtained by
+// The marginal fraction of current that is required to come from PV is the divert_PV_ratio. The requiring a pure PV charge is obtained by
 // setting divert_PV_ratio to 1.0. This is the best choice when the kWh selling price is the same as the kWh night buying price.  If instead the night tarif
-// is the same as the day tarif, any available current is good to take and divert_PV_ratio optimal setting is close to 0.0.
+// is the same as the day tarif, any available current is good to take and divert_PV_ratio optimal setting is close to 0.0. Beyond 1.0, the excess of
+// divert_PV_ratio indicates the amount of power (in kW) that OpenEVSE will try to preserve. A value of 1.1 will start charging only when the
+// PV power - 100 W (reserve) reaches the minimum charging power (reproducing the legacy behavior of OpenEVSE).
 
 // If EVSE is sleeping, charging will start as soon as solar PV / excess power exceeds the divert_PV_ratio fraction of the minimum charging power.
 // Once charging begins it will not pause before a minimum amount of time has passed and this even if solar PV / excess power drops less then minimum charge rate.
 // This avoids wear on the relay and the car.
-#define GRID_IE_RESERVE_POWER (0.0)
 
 // Default to normal charging unless set. Divert mode always defaults back to 1 if unit is reset (divertmode not saved in EEPROM)
-byte divertmode = DIVERT_MODE_NORMAL;     // default normal mode
+byte divertmode = DIVERT_MODE_NORMAL; // default normal mode
 int solar = 0;
 int grid_ie = 0;
-int min_charge_current = 6;      // TO DO: set to be min charge current as set on the OpenEVSE e.g. "$GC min-current max-current"
-int max_charge_current = 32;     // TO DO: to be set to be max charge current as set on the OpenEVSE e.g. "$GC min-current max-current"
+int min_charge_current = 6;  // TO DO: set to be min charge current as set on the OpenEVSE e.g. "$GC min-current max-current"
+int max_charge_current = 32; // TO DO: to be set to be max charge current as set on the OpenEVSE e.g. "$GC min-current max-current"
 int charge_rate = 0;
 int last_state = OPENEVSE_STATE_INVALID;
 uint32_t lastUpdate = 0;
-
 
 double available_current = 0;
 double smoothed_available_current = 0;
@@ -67,42 +67,43 @@ time_t __attribute__((weak)) divertmode_get_time()
 void divertmode_update(byte newmode)
 {
   DBUGF("Set divertmode: %d", newmode);
-  if(divertmode != newmode)
+  if (divertmode != newmode)
   {
     divertmode = newmode;
 
     // restore max charge current if normal mode or zero if eco mode
-    switch(divertmode)
+    switch (divertmode)
     {
-      case DIVERT_MODE_NORMAL:
-        // Restore the max charge current
-        rapiSender.sendCmdSync(String(F("$SC ")) + String(max_charge_current));
-        DBUGF("Restore max I: %d", max_charge_current);
-        break;
+    case DIVERT_MODE_NORMAL:
+      // Restore the max charge current
+      rapiSender.sendCmdSync(String(F("$SC ")) + String(max_charge_current));
+      DBUGF("Restore max I: %d", max_charge_current);
+      break;
 
-      case DIVERT_MODE_ECO:
-        charge_rate = 0;
-        available_current = 0;
-        smoothed_available_current = 0;
-        min_charge_end = 0;
-        
-        // Read the current charge current, assume this is the max set by the user
-        if(0 == rapiSender.sendCmdSync(F("$GE"))) {
-          max_charge_current = String(rapiSender.getToken(1)).toInt();
-          DBUGF("Read max I: %d", max_charge_current);
-        }
-        if(OPENEVSE_STATE_SLEEPING != state)
+    case DIVERT_MODE_ECO:
+      charge_rate = 0;
+      available_current = 0;
+      smoothed_available_current = 0;
+      min_charge_end = 0;
+
+      // Read the current charge current, assume this is the max set by the user
+      if (0 == rapiSender.sendCmdSync(F("$GE")))
+      {
+        max_charge_current = String(rapiSender.getToken(1)).toInt();
+        DBUGF("Read max I: %d", max_charge_current);
+      }
+      if (OPENEVSE_STATE_SLEEPING != state)
+      {
+        if (0 == rapiSender.sendCmdSync(config_pause_uses_disabled() ? F("$FD") : F("$FS")))
         {
-          if(0 == rapiSender.sendCmdSync(config_pause_uses_disabled() ? F("$FD") : F("$FS"))) 
-          {
-            DBUGLN(F("Divert activated, entered sleep mode"));
-            divert_active = false;
-          }
+          DBUGLN(F("Divert activated, entered sleep mode"));
+          divert_active = false;
         }
-        break;
+      }
+      break;
 
-      default:
-        return;
+    default:
+      return;
     }
 
     StaticJsonDocument<128> event;
@@ -127,9 +128,12 @@ void divert_update_state()
   StaticJsonDocument<128> event;
   event["divert_update"] = 0;
 
-  if(mqtt_grid_ie != "") {
+  if (mqtt_grid_ie != "")
+  {
     event["grid_ie"] = grid_ie;
-  } else {
+  }
+  else
+  {
     event["solar"] = solar;
   }
 
@@ -139,7 +143,8 @@ void divert_update_state()
     int current_charge_rate = charge_rate;
 
     // Read the current charge rate
-    if(0 == rapiSender.sendCmdSync(F("$GE"))) {
+    if (0 == rapiSender.sendCmdSync(F("$GE")))
+    {
       current_charge_rate = String(rapiSender.getToken(1)).toInt();
       DBUGVAR(current_charge_rate);
     }
@@ -157,7 +162,8 @@ void divert_update_state()
       DBUGVAR(Igrid_ie);
 
       // Subtract the current charge the EV is using from the Grid IE
-      if(0 == rapiSender.sendCmdSync(F("$GG"))) {
+      if (0 == rapiSender.sendCmdSync(F("$GG")))
+      {
         int milliAmps = String(rapiSender.getToken(1)).toInt();
         double amps = (double)milliAmps / 1000.0;
         DBUGVAR(amps);
@@ -168,7 +174,7 @@ void divert_update_state()
       if (Igrid_ie < 0)
       {
         // If excess power
-        double reserve = GRID_IE_RESERVE_POWER / voltage;
+        double reserve = (1000.0 * ((divert_PV_ratio > 1.0) ? (divert_PV_ratio - 1.0) : 0.0)) / voltage;
         DBUGVAR(reserve);
         available_current = (-Igrid_ie - reserve);
       }
@@ -178,14 +184,15 @@ void divert_update_state()
         available_current = 0;
       }
     }
-    else if (mqtt_solar!="")
+    else if (mqtt_solar != "")
     {
       // if grid feed is not available: charge rate = solar generation
       DBUGVAR(voltage);
       available_current = (double)solar / voltage;
     }
 
-    if(available_current < 0) {
+    if (available_current < 0)
+    {
       available_current = 0;
     }
     DBUGVAR(available_current);
@@ -196,12 +203,13 @@ void divert_update_state()
 
     charge_rate = (int)floor(available_current);
     // if the remaining current can be used with a sufficient ratio of PV current in it, use it
-    if ((available_current - charge_rate) > divert_PV_ratio)
+    if ((available_current - charge_rate) > min(1.0, divert_PV_ratio))
     {
       charge_rate += 1;
     }
 
-    if(OPENEVSE_STATE_SLEEPING != state) {
+    if (OPENEVSE_STATE_SLEEPING != state)
+    {
       // If we are not sleeping, make sure we are the minimum current
       charge_rate = max(charge_rate, static_cast<int>(min_charge_current));
     }
@@ -209,25 +217,28 @@ void divert_update_state()
     DBUGVAR(charge_rate);
 
     // the smoothed current suffices to ensure a sufficient ratio of PV power
-    if (smoothed_available_current >= (min_charge_current * divert_PV_ratio))
+    if (smoothed_available_current >= (min_charge_current * min(1.0, divert_PV_ratio)))
     {
       // Cap the charge rate at the configured maximum
       charge_rate = min(charge_rate, static_cast<int>(max_charge_current));
 
       // Change the charge rate if needed
-      if(current_charge_rate != charge_rate)
+      if (current_charge_rate != charge_rate)
       {
         // Set charge rate via RAPI
         bool chargeRateSet = false;
         // Try and set current with new API with volatile flag (don't save the current rate to EEPROM)
-        if(0 == rapiSender.sendCmdSync(String(F("$SC ")) + String(charge_rate) + String(F(" V")))) {
+        if (0 == rapiSender.sendCmdSync(String(F("$SC ")) + String(charge_rate) + String(F(" V"))))
+        {
           chargeRateSet = true;
-        } else if(0 == rapiSender.sendCmdSync(String(F("$SC ")) + String(charge_rate))) {
+        }
+        else if (0 == rapiSender.sendCmdSync(String(F("$SC ")) + String(charge_rate)))
+        {
           // Fallback to old API
           chargeRateSet = true;
         }
 
-        if(true == chargeRateSet) 
+        if (true == chargeRateSet)
         {
           DBUGF("Charge rate set to %d", charge_rate);
           pilot = charge_rate;
@@ -241,29 +252,31 @@ void divert_update_state()
         bool chargeStarted = false;
 
         // Check if the timer is enabled, we need to do a bit of hackery if it is
-        if(0 == rapiSender.sendCmdSync("$GD"))
+        if (0 == rapiSender.sendCmdSync("$GD"))
         {
-          if(rapiSender.getTokenCnt() >= 5 &&
-             (0 != String(rapiSender.getToken(1)).toInt() ||
-              0 != String(rapiSender.getToken(2)).toInt() ||
-              0 != String(rapiSender.getToken(3)).toInt() ||
-              0 != String(rapiSender.getToken(4)).toInt()))
+          if (rapiSender.getTokenCnt() >= 5 &&
+              (0 != String(rapiSender.getToken(1)).toInt() ||
+               0 != String(rapiSender.getToken(2)).toInt() ||
+               0 != String(rapiSender.getToken(3)).toInt() ||
+               0 != String(rapiSender.getToken(4)).toInt()))
           {
             // Timer is enabled so we need to emulate a button press to work around
             // an issue with $FE not working
-            if(false == chargeStarted && 0 == rapiSender.sendCmdSync(F("$F1"))) {
+            if (false == chargeStarted && 0 == rapiSender.sendCmdSync(F("$F1")))
+            {
               DBUGLN(F("Starting charge with button press"));
               chargeStarted = true;
             }
           }
         }
 
-        if(false == chargeStarted && 0 == rapiSender.sendCmdSync(F("$FE"))) {
+        if (false == chargeStarted && 0 == rapiSender.sendCmdSync(F("$FE")))
+        {
           DBUGLN(F("Starting charge"));
           chargeStarted = true;
         }
 
-        if(chargeStarted) 
+        if (chargeStarted)
         {
           min_charge_end = divertmode_get_time() + divert_min_charge_time;
           event["divert_active"] = divert_active = true;
@@ -272,16 +285,17 @@ void divert_update_state()
     }
     else
     {
-      if(OPENEVSE_STATE_SLEEPING != state)
+      if (OPENEVSE_STATE_SLEEPING != state)
       {
-        if(divert_active && divertmode_get_time() >= min_charge_end) 
+        if (divert_active && divertmode_get_time() >= min_charge_end)
         {
-          if(0 == rapiSender.sendCmdSync(config_pause_uses_disabled() ? F("$FD") : F("$FS"))) 
+          if (0 == rapiSender.sendCmdSync(config_pause_uses_disabled() ? F("$FD") : F("$FS")))
           {
             DBUGLN(F("Charge Stopped"));
             event["divert_active"] = divert_active = false;
 
-            if(0 == rapiSender.sendCmdSync(String(F("$SC ")) + String(max_charge_current))) {
+            if (0 == rapiSender.sendCmdSync(String(F("$SC ")) + String(max_charge_current)))
+            {
               DBUGF("Restore max I: %d", max_charge_current);
             }
           }
