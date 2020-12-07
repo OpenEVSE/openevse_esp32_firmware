4dc4278eae0822bde4507d40c95509d86ad8ca4b
Dan McGee <dan@archlinux.org>
2020-12-06 20:26:22 -0600

    Don't allow voltage to be set <= 0.0 via MQTT
    Voltage is used by divert mode, as well as sent along to the OpenEVSE
    module for power calculations.
    
    Setting this to zero could result in a lot of issues. The divert code
    would see divide by zero happen, and the energy montoring code on the
    OpenEVSE module would result in the recorded energy usage being
    incorrect.
    
    While this shouldn't happen reguarly, I found myself in this situation
    by sending a null/zero-length message to the relevant topic that the
    ESP32 was listening on. Because there is no error checking in the float
    parsing code, a null value parsed to zero, which resulted in the voltage
    value being set to zero.
    
    An alternative here would be to reset the voltage to `DEFAULT_VOLTAGE`
    if a zero or invalid value was received. As written, this will simply do
    nothing and ignore the invalid/zero value.

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -57,11 +57,16 @@ void mqttmsg_callback(MongooseString topic, MongooseString payload) {
     divert_update_state();
   }
   else if (topic_string == mqtt_vrms){
-    voltage = payload_str.toFloat();
-    DBUGF("voltage:%.1f", voltage);
-    OpenEVSE.setVoltage(voltage, [](int ret) {
-      // Only gives better power calculations so not critical if this fails
-    });
+    double volts = payload_str.toFloat();
+    if (volts > 0.0) {
+      voltage = volts;
+      DBUGF("voltage:%.1f", voltage);
+      OpenEVSE.setVoltage(voltage, [](int ret) {
+        // Only gives better power calculations so not critical if this fails
+      });
+    } else {
+      DBUGLN("Ignoring voltage (was <= 0.0)")
+    }
   }
   // If MQTT message to set divert mode is received
   else if (topic_string == mqtt_topic + "/divertmode/set"){
