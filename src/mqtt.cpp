cdb5a826cfbc9077f8a87567d0ce07578a7bf92d
Dan McGee <dan@archlinux.org>
2020-12-09 08:51:40 -0600

    Restrict voltage via MQTT to a known sane range
    A value between 60 and 300 volts, while being a generous range, should
    prevent issues where the value read from MQTT is read as a `0` or
    erroneously specified in millivolts.

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -58,14 +58,14 @@ void mqttmsg_callback(MongooseString topic, MongooseString payload) {
   }
   else if (topic_string == mqtt_vrms){
     double volts = payload_str.toFloat();
-    if (volts > 0.0) {
+    if (volts >= 60.0 && volts <= 300.0) {
       voltage = volts;
-      DBUGF("voltage:%.1f", voltage);
-      OpenEVSE.setVoltage(voltage, [](int ret) {
+      DBUGF("voltage:%.1f", volts);
+      OpenEVSE.setVoltage(volts, [](int ret) {
         // Only gives better power calculations so not critical if this fails
       });
     } else {
-      DBUGLN("Ignoring voltage (was <= 0.0)")
+      DBUGF("voltage:%.1f (ignoring, out of range)", volts);
     }
   }
   // If MQTT message to set divert mode is received
