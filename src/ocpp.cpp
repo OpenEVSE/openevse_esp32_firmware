c4f412f5fab5f90b328bcf4107af9d20498b347a
matth-x <63792403+matth-x@users.noreply.github.com>
2021-06-02 21:47:05 +0200

    more readable claiming procedure

--- src/ocpp.cpp
+++ src/ocpp.cpp
@@ -81,6 +81,10 @@ void ArduinoOcppTask::loadEvseBehavior() {
         return;
     }
 
+    /*
+     * Synchronize OpenEVSE data with OCPP-library data
+     */
+
     setPowerActiveImportSampler([evse = evse]() {
         return (float) (evse->getAmps() * evse->getVoltage());
     });
@@ -103,7 +107,7 @@ void ArduinoOcppTask::loadEvseBehavior() {
     });
 
     /*
-     * CP behavior definition
+     * CP behavior definition: How will plugging and unplugging the EV start or stop OCPP transactions
      */
 
     if (!config_ocpp_enabled()) {
@@ -157,7 +161,11 @@ void ArduinoOcppTask::loadEvseBehavior() {
             return;
         }
 
-        onVehicleConnect();
+        startTransaction([this] (JsonObject payload) {
+            this->updateEvseClaim();
+        }, [lcd = lcd] () {
+            LCD_DISPLAY("Central system error");
+        });
     });
 
     onVehicleDisconnect = [this] () {
@@ -176,51 +184,6 @@ void ArduinoOcppTask::loadEvseBehavior() {
         stopTransaction();
         this->updateEvseClaim();
     });
-
-    if (config_ocpp_access_can_energize()) {
-        inferClaimTransactionActive = [] (EvseState& evseState, EvseProperties& evseProperties) {
-            evseState = EvseState::Active;
-            evseProperties.setState(evseState);
-        };
-    } else {
-        inferClaimTransactionActive = [] (EvseState& evseState, EvseProperties& evseProperties) {};
-    }
-
-    if (config_ocpp_access_can_suspend()) {
-        inferClaimTransactionInactive = [] (EvseState& evseState, EvseProperties& evseProperties) {
-            evseState = EvseState::Disabled;
-            evseProperties.setState(evseState);
-        };
-    } else {
-        inferClaimTransactionInactive = [] (EvseState& evseState, EvseProperties& evseProperties) {};
-    }
-
-    if (!strcmp(tx_start_point.c_str(), "tx_pending")) {
-        inferClaimTransactionActiveOffline = inferClaimTransactionActive;
-    } else {
-        inferClaimTransactionActiveOffline = inferClaimTransactionInactive;
-    }
-
-    if (!strcmp(tx_start_point.c_str(), "tx_only_remote")) {
-        onVehicleConnect = [] () {}; //plugging EV physically will be ignored, but the RemoteStartTransaction has captured a working copy before
-        inferClaimTransactionActiveOffline = inferClaimTransactionInactive; //unlikely to happen
-    }
-
-    inferClaimSmartCharging = [evse = evse, &inferClaimTransactionInactive = inferClaimTransactionInactive] (EvseState& evseState, EvseProperties& evseProperties, float charging_limit) {
-        if (charging_limit < 0.f) {
-            //OCPP Smart Charging is off. Nothing to do
-        } else if (charging_limit >= -0.001f && charging_limit < 5.f) {
-            //allowed charge rate is "equal or almost equal" to 0W
-            inferClaimTransactionInactive(evseState, evseProperties);
-        } else {
-            //charge rate is valid. Set charge rate
-            float volts = evse->getVoltage(); // convert Watts to Amps. TODO Maybe use "smoothed" voltage value?
-            if (volts > 0) {
-                float amps = charging_limit / volts;
-                evseProperties.setChargeCurrent(amps);
-            }
-        }
-    };
 
     updateEvseClaim();
 }
@@ -241,15 +204,14 @@ unsigned long ArduinoOcppTask::loop(MicroTasks::WakeReason reason) {
     if (evse->isVehicleConnected() && !vehicleConnected) {
         vehicleConnected = evse->isVehicleConnected();
 
-        //transition: no EV plugged -> EV plugged
-
-        onVehicleConnect();
+        if (strcmp(tx_start_point.c_str(), "tx_only_remote")) {
+            //tx_start_point is different from tx_only_remote. Plugging an EV starts a transaction
+            onVehicleConnect();
+        }
     }
 
     if (!evse->isVehicleConnected() && vehicleConnected) {
         vehicleConnected = evse->isVehicleConnected();
-
-        //transition: EV plugged -> no EV plugged
 
         onVehicleDisconnect();
     }
@@ -268,20 +230,27 @@ void ArduinoOcppTask::OcppLibrary_loop() {
 }
 
 void ArduinoOcppTask::updateEvseClaim() {
-#if 0 // Simpler approach. Equivalent algorithm with strategy pattern in else clause (preferred)
+
     EvseState evseState;
     EvseProperties evseProperties;
 
-    int transactionId = getTransactionId(); //ID of OCPP-transaction. transactionId <= 0 means that no transaction runs on the EVSE at the moment
+    int transactionId = getTransactionId(); //ID of OCPP-transaction. transactionId <  0 means that no transaction runs on the EVSE at the moment
                                             //                        transactionId >  0 means that the EVSE is in a charging transaction right now
-                                            //                        transactionId == 0 is invalid
+                                            //                        transactionId == 0 means that the initiation is pending
 
     //EVSE is in an OCPP-transaction?
     if (transactionId < 0) {
         //no transaction running. Forbid charging
         evseState = EvseState::Disabled;
+    } else if (transactionId == 0) {
+        //transaction initiated but neither accepted nor rejected
+        if (!strcmp(tx_start_point.c_str(), "tx_pending")) {
+            evseState = EvseState::Active;
+        } else {
+            evseState = EvseState::Disabled;
+        }
     } else {
-        //transaction running or transactionId invalid. Allow charging
+        //transaction running. Allow charging
         evseState = EvseState::Active;
     }
 
@@ -290,7 +259,7 @@ void ArduinoOcppTask::updateEvseClaim() {
     //OCPP Smart Charging?
     if (charging_limit < 0.f) {
         //OCPP Smart Charging is off. Nothing to do
-    } else if (charging_limit >= 0.f && charging_limit < 50.f) {
+    } else if (charging_limit >= -0.001f && charging_limit < 5.f) {
         //allowed charge rate is "equal or almost equal" to 0W
         evseState = EvseState::Disabled; //override state
         evseProperties = evseState; //renew properties
@@ -303,33 +272,20 @@ void ArduinoOcppTask::updateEvseClaim() {
         }
     }
 
-    evse->claim(EvseClient_OpenEVSE_Ocpp, EvseManager_Priority_Ocpp, evseProperties);
-#else
-
-    EvseState evseState = EvseState::None;
-    EvseProperties evseProperties = evseState;
-    
-    int transactionId = getTransactionId(); //ID of OCPP-transaction. transactionId <= 0 means that no transaction runs on the EVSE at the moment
-                                            //                        transactionId >  0 means that the EVSE is in a charging transaction right now
-                                            //                        transactionId == 0 means that the initiation is pending
-
-    //EVSE is in an OCPP-transaction?
-    if (transactionId < 0) {
-        //no transaction running. Forbid charging
-        inferClaimTransactionInactive(evseState, evseProperties);
-    } else if (transactionId == 0) {
-        //transaction initiated but neither accepted nor rejected
-        inferClaimTransactionActiveOffline(evseState, evseProperties);
-    } else {
-        //transaction running. Allow charging
-        inferClaimTransactionActive(evseState, evseProperties);
+    if (evseState == EvseState::Disabled && !config_ocpp_access_can_suspend()) {
+        //OCPP is configured to never put the EVSE into sleep
+        evseState = EvseState::None;
+        evseProperties = evseState;
     }
 
-    //OCPP Smart Charging?
-    inferClaimSmartCharging(evseState, evseProperties, charging_limit);
+    if (evseState == EvseState::Active && !config_ocpp_access_can_energize()) {
+        //OCPP is configured to never override the sleep mode of other services
+        evseState = EvseState::None;
+        evseProperties = evseState;
+    }
 
     //Apply inferred claim
-    if (evseState == EvseState::None) {
+    if (evseState == EvseState::None || !config_ocpp_enabled()) {
         //the claiming rules don't specify the EVSE state
         evse->release(EvseClient_OpenEVSE_Ocpp);
     } else {
@@ -337,7 +293,6 @@ void ArduinoOcppTask::updateEvseClaim() {
         evse->claim(EvseClient_OpenEVSE_Ocpp, EvseManager_Priority_Ocpp, evseProperties);
     }
 
-#endif
 }
 
 String ArduinoOcppTask::getCentralSystemUrl() {
