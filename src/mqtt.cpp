73f9d614ba79d5a580eee2c55e8e871c480be886
KipK <hotlobster75@gmail.com>
2022-06-24 22:00:11 +0200

    remove max_current/set topic subscription

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -274,9 +274,6 @@ mqtt_connect()
     mqtt_sub_topic = mqtt_topic + "/divertmode/set";
     mqttclient.subscribe(mqtt_sub_topic);
 
-    mqtt_sub_topic = mqtt_topic + "/max_current/set";     
-    mqttclient.subscribe(mqtt_sub_topic);
-
     mqtt_sub_topic = mqtt_topic + "/override/set";        
     mqttclient.subscribe(mqtt_sub_topic);
 
