feaee8ce277f625d431dc8b7ded5f7a9de9169bc
Jeremy Poulter <jpoulter@webgains.com>
2022-08-06 22:09:34 +0100

    Fixed issue with claims that don't set the state
    If there was only a single claim that didn't set the state the EVSE manager would not defalut to active, this was only done when there where no claims. This was not a problem for previous modes that always set the state one way or another but for the current shaper it only sets the current and does not set the state, so if it is the only mode in operation and it switched off the charge it would not recover untill something else activated the charge.

--- src/evse_man.cpp
+++ src/evse_man.cpp
@@ -66,15 +66,15 @@ bool EvseProperties::deserialize(JsonObject &obj)
 
   if(obj.containsKey("max_current")) {
     obj["max_current"] == "clear" ? _max_current = UINT32_MAX : _max_current = obj["max_current"];
-  } 
-   
+  }
+
 
   if(obj.containsKey("energy_limit")) {
     obj["energy_limit"] == "clear" ? _energy_limit = UINT32_MAX : _energy_limit = obj["energy_limit"];
   }
 
   if(obj.containsKey("time_limit")) {
-    obj["time_limit"] == "clear" ? _time_limit = UINT32_MAX : _time_limit = obj["time_limit"]; 
+    obj["time_limit"] == "clear" ? _time_limit = UINT32_MAX : _time_limit = obj["time_limit"];
   }
 
   if(obj.containsKey("auto_release")) {
@@ -186,7 +186,9 @@ bool EvseManager::findClaim(EvseClient client, Claim **claim)
 
 bool EvseManager::evaluateClaims(EvseProperties &properties)
 {
+  // Clear the target state and set to active by default
   properties.clear();
+  properties.setState(EvseState::Active);
 
   bool foundClaim = false;
 
@@ -401,16 +403,11 @@ unsigned long EvseManager::loop(MicroTasks::WakeReason reason)
     _hasClaims = evaluateClaims(_targetProperties);
 
     DBUGVAR(_hasClaims);
-    if(_hasClaims)
-    {
-      DBUGVAR(_targetProperties.getState().toString());
-      DBUGVAR(_targetProperties.getChargeCurrent());
-      DBUGVAR(_targetProperties.getMaxCurrent());
-      DBUGVAR(_targetProperties.getEnergyLimit());
-      DBUGVAR(_targetProperties.getTimeLimit());
-    } else {
-      DBUGLN("No claims");
-    }
+    DBUGVAR(_targetProperties.getState().toString());
+    DBUGVAR(_targetProperties.getChargeCurrent());
+    DBUGVAR(_targetProperties.getMaxCurrent());
+    DBUGVAR(_targetProperties.getEnergyLimit());
+    DBUGVAR(_targetProperties.getTimeLimit());
 
     _evaluateTargetState = true;
   }
@@ -419,13 +416,6 @@ unsigned long EvseManager::loop(MicroTasks::WakeReason reason)
   if(_evaluateTargetState)
   {
     _evaluateTargetState = false;
-
-    if(!_hasClaims)
-    {
-      // No claims, make sure the targetProperties are the defaults with charger active
-      _targetProperties.clear();
-      _targetProperties.setState(EvseState::Active);
-    }
     setTargetState(_targetProperties);
   }
 
