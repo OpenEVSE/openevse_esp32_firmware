57b0c93a2bcdf28e250be06707ad2abb6bad1d06
KipK <hotlobster75@gmail.com>
2023-05-21 23:59:08 +0200

    updated gui statics

---
+++ src/web_static/web_server.success_html.h
@@ -0,0 +1,16 @@
+static const char CONTENT_SUCCESS_HTML[] PROGMEM = 
+  "<HTML>\n"
+  "<HEAD>\n"
+  "	<TITLE>Success</TITLE>\n"
+  "	<script type=\"text/javascript\">\n"
+  "	window.onload = function() {\n"
+  "		window.location.href = \"http://\" + window.location.host;\n"
+  "	}\n"
+  "	</script>\n"
+  "</HEAD>\n"
+  "<BODY>\n"
+  "Success\n"
+  "</BODY>\n"
+  "\n"
+  "</HTML>\n";
+static const char CONTENT_SUCCESS_HTML_ETAG[] PROGMEM = "1e5004f6ad6d59e54b73c7f3881ce08df6e77c1ddc6369e59eb386e45b98dc74";
--- src/web_static/web_server_static_files.h
+++ src/web_static/web_server_static_files.h
@@ -1,2 +1,38 @@
+#include "web_server.assets_components_881820ae_js_gz.h"
+#include "web_server.assets_components_a2a8801e_css_gz.h"
+#include "web_server.assets_config_dcddfc33_css_gz.h"
+#include "web_server.assets_config_f514668a_js_gz.h"
+#include "web_server.assets_en_e1685b8a_js_gz.h"
+#include "web_server.assets_fr_2f7af51e_js_gz.h"
+#include "web_server.assets_icons_dfef1ccc_js_gz.h"
+#include "web_server.assets_index_2ef8da85_js_gz.h"
+#include "web_server.assets_index_91444eb6_css_gz.h"
+#include "web_server.assets_logo_mini_e4e21c4b_png.h"
+#include "web_server.assets_vendor_018e9e8e_js_gz.h"
+#include "web_server.favicon_ico.h"
+#include "web_server.index_html_gz.h"
+#include "web_server.manifest_webmanifest.h"
+#include "web_server.pwa_192x192_png.h"
+#include "web_server.pwa_masquable_png.h"
+#include "web_server.success_html.h"
+#include "web_server.sw_js.h"
 StaticFile staticFiles[] = {
+  { "/assets/components-881820ae.js", CONTENT_COMPONENTS_881820AE_JS_GZ, sizeof(CONTENT_COMPONENTS_881820AE_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_COMPONENTS_881820AE_JS_GZ_ETAG, true },
+  { "/assets/components-a2a8801e.css", CONTENT_COMPONENTS_A2A8801E_CSS_GZ, sizeof(CONTENT_COMPONENTS_A2A8801E_CSS_GZ) - 1, _CONTENT_TYPE_CSS, CONTENT_COMPONENTS_A2A8801E_CSS_GZ_ETAG, true },
+  { "/assets/config-dcddfc33.css", CONTENT_CONFIG_DCDDFC33_CSS_GZ, sizeof(CONTENT_CONFIG_DCDDFC33_CSS_GZ) - 1, _CONTENT_TYPE_CSS, CONTENT_CONFIG_DCDDFC33_CSS_GZ_ETAG, true },
+  { "/assets/config-f514668a.js", CONTENT_CONFIG_F514668A_JS_GZ, sizeof(CONTENT_CONFIG_F514668A_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_CONFIG_F514668A_JS_GZ_ETAG, true },
+  { "/assets/en-e1685b8a.js", CONTENT_EN_E1685B8A_JS_GZ, sizeof(CONTENT_EN_E1685B8A_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_EN_E1685B8A_JS_GZ_ETAG, true },
+  { "/assets/fr-2f7af51e.js", CONTENT_FR_2F7AF51E_JS_GZ, sizeof(CONTENT_FR_2F7AF51E_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_FR_2F7AF51E_JS_GZ_ETAG, true },
+  { "/assets/icons-dfef1ccc.js", CONTENT_ICONS_DFEF1CCC_JS_GZ, sizeof(CONTENT_ICONS_DFEF1CCC_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_ICONS_DFEF1CCC_JS_GZ_ETAG, true },
+  { "/assets/index-2ef8da85.js", CONTENT_INDEX_2EF8DA85_JS_GZ, sizeof(CONTENT_INDEX_2EF8DA85_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_INDEX_2EF8DA85_JS_GZ_ETAG, true },
+  { "/assets/index-91444eb6.css", CONTENT_INDEX_91444EB6_CSS_GZ, sizeof(CONTENT_INDEX_91444EB6_CSS_GZ) - 1, _CONTENT_TYPE_CSS, CONTENT_INDEX_91444EB6_CSS_GZ_ETAG, true },
+  { "/assets/logo-mini-e4e21c4b.png", CONTENT_LOGO_MINI_E4E21C4B_PNG, sizeof(CONTENT_LOGO_MINI_E4E21C4B_PNG) - 1, _CONTENT_TYPE_PNG, CONTENT_LOGO_MINI_E4E21C4B_PNG_ETAG, false },
+  { "/assets/vendor-018e9e8e.js", CONTENT_VENDOR_018E9E8E_JS_GZ, sizeof(CONTENT_VENDOR_018E9E8E_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_VENDOR_018E9E8E_JS_GZ_ETAG, true },
+  { "/favicon.ico", CONTENT_FAVICON_ICO, sizeof(CONTENT_FAVICON_ICO) - 1, _CONTENT_TYPE_ICO, CONTENT_FAVICON_ICO_ETAG, false },
+  { "/index.html", CONTENT_INDEX_HTML_GZ, sizeof(CONTENT_INDEX_HTML_GZ) - 1, _CONTENT_TYPE_HTML, CONTENT_INDEX_HTML_GZ_ETAG, true },
+  { "/manifest.webmanifest", CONTENT_MANIFEST_WEBMANIFEST, sizeof(CONTENT_MANIFEST_WEBMANIFEST) - 1, _CONTENT_TYPE_MANIFEST, CONTENT_MANIFEST_WEBMANIFEST_ETAG, false },
+  { "/pwa-192x192.png", CONTENT_PWA_192X192_PNG, sizeof(CONTENT_PWA_192X192_PNG) - 1, _CONTENT_TYPE_PNG, CONTENT_PWA_192X192_PNG_ETAG, false },
+  { "/pwa-masquable.png", CONTENT_PWA_MASQUABLE_PNG, sizeof(CONTENT_PWA_MASQUABLE_PNG) - 1, _CONTENT_TYPE_PNG, CONTENT_PWA_MASQUABLE_PNG_ETAG, false },
+  { "/success.html", CONTENT_SUCCESS_HTML, sizeof(CONTENT_SUCCESS_HTML) - 1, _CONTENT_TYPE_HTML, CONTENT_SUCCESS_HTML_ETAG, false },
+  { "/sw.js", CONTENT_SW_JS, sizeof(CONTENT_SW_JS) - 1, _CONTENT_TYPE_JS, CONTENT_SW_JS_ETAG, false },
 };
