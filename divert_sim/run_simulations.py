38e07dde751f5aa60ff4034d27d1d2f75a199f53
Jeremy Poulter <jpoulter@webgains.com>
2023-05-30 19:14:29 +0100

    Use new CSV file option to read in the CSV data rather than STDIN
    STDIN can still be used via the specia `-` file name

--- divert_sim/run_simulations.py
+++ divert_sim/run_simulations.py
@@ -60,87 +60,86 @@ def run_simulation(dataset: str,
     print("Testing dataset: " + dataset)
 
     # Read in the dataset and pass to the divert_sim process
-    with open(path.join('data', dataset+'.csv'), 'r', encoding="utf-8") as input_data:
-        with open(path.join('output', output+'.csv'), 'w', encoding="utf-8") as output_data:
-            # open the divert_sim process
-            command = ["./divert_sim"]
-            if config:
-                command.append("-c")
-                command.append(config)
-            if grid_ie_col:
-                command.append("-g")
-                command.append(str(grid_ie_col))
-            if solar_col:
-                command.append("-s")
-                command.append(str(solar_col))
-            if voltage_col:
-                command.append("-v")
-                command.append(str(voltage_col))
-            if separator:
-                command.append("--sep")
-                command.append(separator)
-            if is_kw:
-                command.append("--kw")
+    with open(path.join('output', output+'.csv'), 'w', encoding="utf-8") as output_data:
+        # open the divert_sim process
+        command = ["./divert_sim", "-i", path.join('data', dataset+'.csv') ]
+        if config:
+            command.append("-c")
+            command.append(config)
+        if grid_ie_col:
+            command.append("-g")
+            command.append(str(grid_ie_col))
+        if solar_col:
+            command.append("-s")
+            command.append(str(solar_col))
+        if voltage_col:
+            command.append("-v")
+            command.append(str(voltage_col))
+        if separator:
+            command.append("--sep")
+            command.append(separator)
+        if is_kw:
+            command.append("--kw")
 
-            divert_process = Popen(command, stdin=input_data, stdout=PIPE,
-                    stderr=PIPE, universal_newlines=True)
-            while True:
-                output = divert_process.stdout.readline()
-                if output == '' and divert_process.poll() is not None:
-                    break
-                if output:
-                    output_data.write(output)
-                    line_number += 1
-                    if line_number > 1:
-                        # read in the csv line
-                        csv_line = output.split(',')
-                        date = datetime.strptime(csv_line[0], '%d/%m/%Y %H:%M:%S')
-                        solar = float(csv_line[1])
-                        # grid_ie = float(csv_line[2])
-                        # pilot = int(csv_line[3])
-                        charge_power = float(csv_line[4])
-                        # min_charge_power = float(csv_line[5])
-                        state = int(csv_line[6])
-                        # smoothed_available = float(csv_line[7])
+        divert_process = Popen(command, stdin=PIPE, stdout=PIPE,
+                stderr=PIPE, universal_newlines=True)
+        while True:
+            output = divert_process.stdout.readline()
+            if output == '' and divert_process.poll() is not None:
+                break
+            if output:
+                output_data.write(output)
+                line_number += 1
+                if line_number > 1:
+                    # read in the csv line
+                    csv_line = output.split(',')
+                    date = datetime.strptime(csv_line[0], '%d/%m/%Y %H:%M:%S')
+                    solar = float(csv_line[1])
+                    # grid_ie = float(csv_line[2])
+                    # pilot = int(csv_line[3])
+                    charge_power = float(csv_line[4])
+                    # min_charge_power = float(csv_line[5])
+                    state = int(csv_line[6])
+                    # smoothed_available = float(csv_line[7])
 
-                        if last_date is not None:
-                            # Get the difference between this date and last date
-                            diff = date - last_date
+                    if last_date is not None:
+                        # Get the difference between this date and last date
+                        diff = date - last_date
 
-                            hours = diff.seconds / 3600
+                        hours = diff.seconds / 3600
 
-                            total_solar_wh += solar * hours
-                            ev_wh = charge_power * hours
-                            total_ev_wh += charge_power * hours
-                            charge_from_solar_wh = min(solar, charge_power) * hours
-                            wh_from_solar += charge_from_solar_wh
-                            wh_from_grid += ev_wh - charge_from_solar_wh
+                        total_solar_wh += solar * hours
+                        ev_wh = charge_power * hours
+                        total_ev_wh += charge_power * hours
+                        charge_from_solar_wh = min(solar, charge_power) * hours
+                        wh_from_solar += charge_from_solar_wh
+                        wh_from_grid += ev_wh - charge_from_solar_wh
 
-                            if state == OPENEVSE_STATE_CHARGING and last_state != OPENEVSE_STATE_CHARGING:
-                                number_of_charges += 1
-                                charge_start_date = date
+                        if state == OPENEVSE_STATE_CHARGING and last_state != OPENEVSE_STATE_CHARGING:
+                            number_of_charges += 1
+                            charge_start_date = date
 
-                            if state != OPENEVSE_STATE_CHARGING and last_state == OPENEVSE_STATE_CHARGING:
-                                this_session_time_charging = (date - charge_start_date).seconds
-                                total_time_charging += this_session_time_charging
-                                if min_time_charging == 0 or this_session_time_charging < min_time_charging:
-                                    min_time_charging = this_session_time_charging
-                                if this_session_time_charging > max_time_charging:
-                                    max_time_charging = this_session_time_charging
+                        if state != OPENEVSE_STATE_CHARGING and last_state == OPENEVSE_STATE_CHARGING:
+                            this_session_time_charging = (date - charge_start_date).seconds
+                            total_time_charging += this_session_time_charging
+                            if min_time_charging == 0 or this_session_time_charging < min_time_charging:
+                                min_time_charging = this_session_time_charging
+                            if this_session_time_charging > max_time_charging:
+                                max_time_charging = this_session_time_charging
 
-                        last_date = date
-                        last_state = state
+                    last_date = date
+                    last_state = state
 
-        solar_kwh=total_solar_wh / 1000
-        ev_kwh=total_ev_wh / 1000
-        kwh_from_solar=wh_from_solar / 1000
-        kwh_from_grid=wh_from_grid / 1000
+    solar_kwh=total_solar_wh / 1000
+    ev_kwh=total_ev_wh / 1000
+    kwh_from_solar=wh_from_solar / 1000
+    kwh_from_grid=wh_from_grid / 1000
 
-        if config is False or config.startswith('{'):
-            config = "Default"
+    if config is False or config.startswith('{'):
+        config = "Default"
 
-        with open(path.join('output', summary_filename), 'a', encoding="utf-8") as summary_file:
-            summary_file.write(f'"{dataset}","{config}",{solar_kwh},{ev_kwh},{kwh_from_solar},{kwh_from_grid},{number_of_charges},{min_time_charging},{max_time_charging},{total_time_charging}\n')
+    with open(path.join('output', summary_filename), 'a', encoding="utf-8") as summary_file:
+        summary_file.write(f'"{dataset}","{config}",{solar_kwh},{ev_kwh},{kwh_from_solar},{kwh_from_grid},{number_of_charges},{min_time_charging},{max_time_charging},{total_time_charging}\n')
 
 
     return (round(solar_kwh, KWH_ROUNDING),
