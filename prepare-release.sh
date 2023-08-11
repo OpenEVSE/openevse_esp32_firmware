d1b68646390b0a21a661a506d38bd8d9b909f5b8
Jeremy Poulter <jpoulter@webgains.com>
2023-08-11 21:16:34 +0100

    More accurate build names Fixes #662

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Script to compile all released enviroments and copy build artifacts to project root for easy upload to github releases. 
+# Script to compile all released enviroments and copy build artifacts to project root for easy upload to github releases.
 
 pio run -e openevse_wifi_v1
 cp .pio/build/openevse_wifi_v1/firmware.bin firmware.bin
 
-pio run -e openevse_huzzah32
-cp .pio/build/openevse_wifi_v1/firmware.bin openevse_huzzah32.bin
+pio run -e adafruit_huzzah32
+cp .pio/build/openevse_wifi_v1/firmware.bin adafruit_huzzah32.bin
 
-pio run -e openevse_esp32-gateway-e
+pio run -e olimex_esp32-gateway-e
 cp .pio/build/openevse_wifi_v1/firmware.bin esp32-gateway-e.bin
 
-echo "Done" 
+echo "Done"
 
 
 
