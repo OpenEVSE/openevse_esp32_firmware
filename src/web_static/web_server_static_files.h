7d7c3b5c66515e342dd1b236dc0be8875dbfd5a3
jeremypoulter <jeremypoulter@users.noreply.github.com>
2024-07-06 21:47:42 +0000

    Update of pre-built GUI files

--- src/web_static/web_server_static_files.h
+++ src/web_static/web_server_static_files.h
@@ -1,14 +1,14 @@
-#include "web_server.assets_components_b935525f_js_gz.h"
+#include "web_server.assets_components_a15bdd1b_js_gz.h"
 #include "web_server.assets_components_bb056724_css_gz.h"
-#include "web_server.assets_config_03264f3d_js_gz.h"
 #include "web_server.assets_config_a0694b83_css_gz.h"
-#include "web_server.assets_en_38ae5a2a_js_gz.h"
-#include "web_server.assets_es_df837104_js_gz.h"
-#include "web_server.assets_fr_c549a7f9_js_gz.h"
+#include "web_server.assets_config_ebd650fe_js_gz.h"
+#include "web_server.assets_en_7d3edac2_js_gz.h"
+#include "web_server.assets_es_09a99823_js_gz.h"
+#include "web_server.assets_fr_76601f68_js_gz.h"
 #include "web_server.assets_hu_8280bea7_js_gz.h"
 #include "web_server.assets_icons_f349374d_js_gz.h"
-#include "web_server.assets_index_7671af61_js_gz.h"
-#include "web_server.assets_index_f2fcf92a_css_gz.h"
+#include "web_server.assets_index_ad128439_css_gz.h"
+#include "web_server.assets_index_d0e1b7d1_js_gz.h"
 #include "web_server.assets_logo_mini_e4e21c4b_png.h"
 #include "web_server.assets_vendor_3d58a145_js_gz.h"
 #include "web_server.favicon_ico.h"
@@ -19,17 +19,17 @@
 #include "web_server.success_html.h"
 #include "web_server.sw_js.h"
 StaticFile web_server_static_files[] = {
-  { "/assets/components-b935525f.js", CONTENT_COMPONENTS_B935525F_JS_GZ, sizeof(CONTENT_COMPONENTS_B935525F_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_COMPONENTS_B935525F_JS_GZ_ETAG, true },
+  { "/assets/components-a15bdd1b.js", CONTENT_COMPONENTS_A15BDD1B_JS_GZ, sizeof(CONTENT_COMPONENTS_A15BDD1B_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_COMPONENTS_A15BDD1B_JS_GZ_ETAG, true },
   { "/assets/components-bb056724.css", CONTENT_COMPONENTS_BB056724_CSS_GZ, sizeof(CONTENT_COMPONENTS_BB056724_CSS_GZ) - 1, _CONTENT_TYPE_CSS, CONTENT_COMPONENTS_BB056724_CSS_GZ_ETAG, true },
-  { "/assets/config-03264f3d.js", CONTENT_CONFIG_03264F3D_JS_GZ, sizeof(CONTENT_CONFIG_03264F3D_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_CONFIG_03264F3D_JS_GZ_ETAG, true },
   { "/assets/config-a0694b83.css", CONTENT_CONFIG_A0694B83_CSS_GZ, sizeof(CONTENT_CONFIG_A0694B83_CSS_GZ) - 1, _CONTENT_TYPE_CSS, CONTENT_CONFIG_A0694B83_CSS_GZ_ETAG, true },
-  { "/assets/en-38ae5a2a.js", CONTENT_EN_38AE5A2A_JS_GZ, sizeof(CONTENT_EN_38AE5A2A_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_EN_38AE5A2A_JS_GZ_ETAG, true },
-  { "/assets/es-df837104.js", CONTENT_ES_DF837104_JS_GZ, sizeof(CONTENT_ES_DF837104_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_ES_DF837104_JS_GZ_ETAG, true },
-  { "/assets/fr-c549a7f9.js", CONTENT_FR_C549A7F9_JS_GZ, sizeof(CONTENT_FR_C549A7F9_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_FR_C549A7F9_JS_GZ_ETAG, true },
+  { "/assets/config-ebd650fe.js", CONTENT_CONFIG_EBD650FE_JS_GZ, sizeof(CONTENT_CONFIG_EBD650FE_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_CONFIG_EBD650FE_JS_GZ_ETAG, true },
+  { "/assets/en-7d3edac2.js", CONTENT_EN_7D3EDAC2_JS_GZ, sizeof(CONTENT_EN_7D3EDAC2_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_EN_7D3EDAC2_JS_GZ_ETAG, true },
+  { "/assets/es-09a99823.js", CONTENT_ES_09A99823_JS_GZ, sizeof(CONTENT_ES_09A99823_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_ES_09A99823_JS_GZ_ETAG, true },
+  { "/assets/fr-76601f68.js", CONTENT_FR_76601F68_JS_GZ, sizeof(CONTENT_FR_76601F68_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_FR_76601F68_JS_GZ_ETAG, true },
   { "/assets/hu-8280bea7.js", CONTENT_HU_8280BEA7_JS_GZ, sizeof(CONTENT_HU_8280BEA7_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_HU_8280BEA7_JS_GZ_ETAG, true },
   { "/assets/icons-f349374d.js", CONTENT_ICONS_F349374D_JS_GZ, sizeof(CONTENT_ICONS_F349374D_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_ICONS_F349374D_JS_GZ_ETAG, true },
-  { "/assets/index-7671af61.js", CONTENT_INDEX_7671AF61_JS_GZ, sizeof(CONTENT_INDEX_7671AF61_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_INDEX_7671AF61_JS_GZ_ETAG, true },
-  { "/assets/index-f2fcf92a.css", CONTENT_INDEX_F2FCF92A_CSS_GZ, sizeof(CONTENT_INDEX_F2FCF92A_CSS_GZ) - 1, _CONTENT_TYPE_CSS, CONTENT_INDEX_F2FCF92A_CSS_GZ_ETAG, true },
+  { "/assets/index-ad128439.css", CONTENT_INDEX_AD128439_CSS_GZ, sizeof(CONTENT_INDEX_AD128439_CSS_GZ) - 1, _CONTENT_TYPE_CSS, CONTENT_INDEX_AD128439_CSS_GZ_ETAG, true },
+  { "/assets/index-d0e1b7d1.js", CONTENT_INDEX_D0E1B7D1_JS_GZ, sizeof(CONTENT_INDEX_D0E1B7D1_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_INDEX_D0E1B7D1_JS_GZ_ETAG, true },
   { "/assets/logo-mini-e4e21c4b.png", CONTENT_LOGO_MINI_E4E21C4B_PNG, sizeof(CONTENT_LOGO_MINI_E4E21C4B_PNG) - 1, _CONTENT_TYPE_PNG, CONTENT_LOGO_MINI_E4E21C4B_PNG_ETAG, false },
   { "/assets/vendor-3d58a145.js", CONTENT_VENDOR_3D58A145_JS_GZ, sizeof(CONTENT_VENDOR_3D58A145_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_VENDOR_3D58A145_JS_GZ_ETAG, true },
   { "/favicon.ico", CONTENT_FAVICON_ICO, sizeof(CONTENT_FAVICON_ICO) - 1, _CONTENT_TYPE_ICO, CONTENT_FAVICON_ICO_ETAG, false },
