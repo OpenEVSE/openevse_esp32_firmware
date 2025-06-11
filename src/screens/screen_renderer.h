c2d33f557620090b381b04e777ac6c6b77dea9c3
Jeremy Poulter <jeremy@bigjungle.net>
2025-06-11 20:50:07 +0100

    Fixed text display

--- src/screens/screen_renderer.h
+++ src/screens/screen_renderer.h
@@ -4,6 +4,8 @@
 #include <TFT_eSPI.h>
 #include <PNGdec.h>
 
+#include "lcd_common.h"
+
 // The TFT screen is portrait natively, so we need to rotate it
 #define TFT_SCREEN_WIDTH  TFT_HEIGHT
 #define TFT_SCREEN_HEIGHT TFT_WIDTH
@@ -36,6 +38,12 @@ extern void render_info_box(const char *title, const char *text, int16_t x, int1
 extern void get_scaled_number_value(double value, int precision, const char *unit,
                              char *buffer, size_t size);
 
+// Message line buffer - shared across screens
+extern char _msg[LCD_MAX_LINES][LCD_MAX_LEN + 1];
+extern bool _msg_cleared;
+
+extern void set_message_line(int x, int y, const char *msg, bool clear);
+extern void clear_message_line(int line);
 extern String get_message_line(int line);
 
 #endif // __SCREEN_RENDERER_H
