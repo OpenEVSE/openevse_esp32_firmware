ca4d7a7ddd61eb66f9ec82c92368ad0b49230cee
Jeremy Poulter <jeremy@bigjungle.net>
2026-07-17 17:24:15 +0100

    Embed updated load sharing UI

--- src/web_static/web_server_static_files.h
+++ src/web_static/web_server_static_files.h
@@ -1,12 +1,12 @@
 #include "web_server.apple_touch_icon_png.h"
 #include "web_server.assets_charts_2ynqPV6l_js_gz.h"
 #include "web_server.assets_charts_CnsZ1jie_css_gz.h"
-#include "web_server.assets_en_BeCQwbJT_js_gz.h"
-#include "web_server.assets_es_DA2CooLz_js_gz.h"
-#include "web_server.assets_fr_Db32TWFK_js_gz.h"
-#include "web_server.assets_hu_CKegUH6M_js_gz.h"
-#include "web_server.assets_index_DNF2JCoM_css_gz.h"
-#include "web_server.assets_index_DRIHpSc0_js_gz.h"
+#include "web_server.assets_en_kPmcN9wU_js_gz.h"
+#include "web_server.assets_es_Chbo25Iv_js_gz.h"
+#include "web_server.assets_fr_DeoMYwKl_js_gz.h"
+#include "web_server.assets_hu_BzfLFADt_js_gz.h"
+#include "web_server.assets_index_BDGh_Sna_js_gz.h"
+#include "web_server.assets_index_CP3cxVgN_css_gz.h"
 #include "web_server.assets_rolldown_runtime_Bh1tDfsg_js_gz.h"
 #include "web_server.assets_vendor_C2pyRtw5_js_gz.h"
 #include "web_server.favicon_ico.h"
@@ -20,12 +20,12 @@ StaticFile web_server_static_files[] = {
   { "/apple-touch-icon.png", CONTENT_APPLE_TOUCH_ICON_PNG, sizeof(CONTENT_APPLE_TOUCH_ICON_PNG) - 1, _CONTENT_TYPE_PNG, CONTENT_APPLE_TOUCH_ICON_PNG_ETAG, false },
   { "/assets/charts-2ynqPV6l.js", CONTENT_CHARTS_2YNQPV6L_JS_GZ, sizeof(CONTENT_CHARTS_2YNQPV6L_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_CHARTS_2YNQPV6L_JS_GZ_ETAG, true },
   { "/assets/charts-CnsZ1jie.css", CONTENT_CHARTS_CNSZ1JIE_CSS_GZ, sizeof(CONTENT_CHARTS_CNSZ1JIE_CSS_GZ) - 1, _CONTENT_TYPE_CSS, CONTENT_CHARTS_CNSZ1JIE_CSS_GZ_ETAG, true },
-  { "/assets/en-BeCQwbJT.js", CONTENT_EN_BECQWBJT_JS_GZ, sizeof(CONTENT_EN_BECQWBJT_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_EN_BECQWBJT_JS_GZ_ETAG, true },
-  { "/assets/es-DA2CooLz.js", CONTENT_ES_DA2COOLZ_JS_GZ, sizeof(CONTENT_ES_DA2COOLZ_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_ES_DA2COOLZ_JS_GZ_ETAG, true },
-  { "/assets/fr-Db32TWFK.js", CONTENT_FR_DB32TWFK_JS_GZ, sizeof(CONTENT_FR_DB32TWFK_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_FR_DB32TWFK_JS_GZ_ETAG, true },
-  { "/assets/hu-CKegUH6M.js", CONTENT_HU_CKEGUH6M_JS_GZ, sizeof(CONTENT_HU_CKEGUH6M_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_HU_CKEGUH6M_JS_GZ_ETAG, true },
-  { "/assets/index-DNF2JCoM.css", CONTENT_INDEX_DNF2JCOM_CSS_GZ, sizeof(CONTENT_INDEX_DNF2JCOM_CSS_GZ) - 1, _CONTENT_TYPE_CSS, CONTENT_INDEX_DNF2JCOM_CSS_GZ_ETAG, true },
-  { "/assets/index-DRIHpSc0.js", CONTENT_INDEX_DRIHPSC0_JS_GZ, sizeof(CONTENT_INDEX_DRIHPSC0_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_INDEX_DRIHPSC0_JS_GZ_ETAG, true },
+  { "/assets/en-kPmcN9wU.js", CONTENT_EN_KPMCN9WU_JS_GZ, sizeof(CONTENT_EN_KPMCN9WU_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_EN_KPMCN9WU_JS_GZ_ETAG, true },
+  { "/assets/es-Chbo25Iv.js", CONTENT_ES_CHBO25IV_JS_GZ, sizeof(CONTENT_ES_CHBO25IV_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_ES_CHBO25IV_JS_GZ_ETAG, true },
+  { "/assets/fr-DeoMYwKl.js", CONTENT_FR_DEOMYWKL_JS_GZ, sizeof(CONTENT_FR_DEOMYWKL_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_FR_DEOMYWKL_JS_GZ_ETAG, true },
+  { "/assets/hu-BzfLFADt.js", CONTENT_HU_BZFLFADT_JS_GZ, sizeof(CONTENT_HU_BZFLFADT_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_HU_BZFLFADT_JS_GZ_ETAG, true },
+  { "/assets/index-BDGh_Sna.js", CONTENT_INDEX_BDGH_SNA_JS_GZ, sizeof(CONTENT_INDEX_BDGH_SNA_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_INDEX_BDGH_SNA_JS_GZ_ETAG, true },
+  { "/assets/index-CP3cxVgN.css", CONTENT_INDEX_CP3CXVGN_CSS_GZ, sizeof(CONTENT_INDEX_CP3CXVGN_CSS_GZ) - 1, _CONTENT_TYPE_CSS, CONTENT_INDEX_CP3CXVGN_CSS_GZ_ETAG, true },
   { "/assets/rolldown-runtime-Bh1tDfsg.js", CONTENT_ROLLDOWN_RUNTIME_BH1TDFSG_JS_GZ, sizeof(CONTENT_ROLLDOWN_RUNTIME_BH1TDFSG_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_ROLLDOWN_RUNTIME_BH1TDFSG_JS_GZ_ETAG, true },
   { "/assets/vendor-C2pyRtw5.js", CONTENT_VENDOR_C2PYRTW5_JS_GZ, sizeof(CONTENT_VENDOR_C2PYRTW5_JS_GZ) - 1, _CONTENT_TYPE_JS, CONTENT_VENDOR_C2PYRTW5_JS_GZ_ETAG, true },
   { "/favicon.ico", CONTENT_FAVICON_ICO, sizeof(CONTENT_FAVICON_ICO) - 1, _CONTENT_TYPE_ICO, CONTENT_FAVICON_ICO_ETAG, false },
